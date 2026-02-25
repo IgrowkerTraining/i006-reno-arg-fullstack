f9937e07006c464fb3327bd58739aec26ad7e54d
Marina Victoria Blanco <marivic_05@hotmail.com>
2026-02-25 19:32:43 -0300

    Agrego Reporte IA, sin conexion a back

--- apps/frontend/src/components/layout/DashboardLayout.tsx
+++ apps/frontend/src/components/layout/DashboardLayout.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useMatch } from "react-router-dom";
 
 export const DashboardLayout: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+const matchReporteIA = useMatch("/dashboard/reporte-ia");
 
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 1024px)");
@@ -20,11 +21,12 @@ export const DashboardLayout: React.FC = () => {
   }, []);
 
   const finalCollapsed = isMobile ? true : collapsed;
+  
 
   return (
-    <div className="flex h-screen">
-      <Sidebar collapsed={finalCollapsed} setCollapsed={setCollapsed} isMobile={isMobile} />
-      <main className="flex-1 overflow-auto min-h-screen pt-6 pr-6 lg:p-8">
+    <div className={`flex min-h-screen ${matchReporteIA ? "bg-[#EEF8FA]" : ""}`}>
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} isMobile={isMobile} />
+      <main className="flex-1 overflow-auto pt-6 pr-6 lg:p-8">
         <Outlet />
       </main>
     </div>
