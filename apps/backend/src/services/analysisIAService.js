3f91aa6b3fe3ba46b4e8643771e7acd89ef65b12
Mauro Procopio <limauropro@gmail.com>
2026-03-03 14:31:15 +0000

    Integración Service IA funcional básica

--- apps/backend/src/services/analysisIAService.js
+++ apps/backend/src/services/analysisIAService.js
@@ -1,62 +1,32 @@
-const Project = require('../models/Project');
-const AnalysisIA = require('../models/AnalysisIA');
+
+const snapshotModule = require('../mocks/snapshotMock');
+const snapshotMock = snapshotModule.default;  // 👈 usar el default
+// const Project = require('../models/Project');
+// const AnalysisIA = require('../models/AnalysisIA');
 
 class AnalysisIAService {
 
     static async createIaAnalysis(projectId, month, year) {
-
-        const rawData = await Project.getDataProjectReport(projectId, month, year);
-
-        if (!rawData || rawData.length === 0) {
-            return null;
-        }
-        const projectInfo = await Project.getProjectHeader(projectId);
-
-        const finalPayload = {
-            project: {
-                id: projectInfo.id_proyecto,
-                name: projectInfo.proyecto_nombre,
-                created_at: projectInfo.fecha_registro,
-                art_name: projectInfo.nombre_art || "No especificado",
-                status: projectInfo.estado || "No especificado",
-                description: projectInfo.descripcion,
-                responsible: {
-                    creator: projectInfo.creador_nombre,
-                    creator_role: projectInfo.creador_rol_nombre,
-                },
-            },
-            analysis_period: {
-                month,
-                year
-            },
-            reports: rawData
-        };
-        /*
-        const resultIA = await this.callExternalAI(projectId, month, year, rawData); comentado hasta que se obtena URL API IA
-
-        const savedAnalysis = await AnalysisIA.save(projectId, resultIA);
-
-        return savedAnalysis;
-        */
-
-        return finalPayload; // Retorno el payload que se enviaría a la IA para pruebas
+        const resultIA = await this.callExternalAI(snapshotMock); //comentado hasta que se obtena URL API IA
+        // const resultIA = await this.callExternalAI(requestBody2); //comentado hasta que se obtena URL API IA
+        return await resultIA;
+        //  const savedAnalysis = await AnalysisIA.save(projectId, finalPayload);
+        // return savedAnalysis; 
+        //return finalPayload; // Retorno el payload que se enviaría a la IA para pruebas
     }
-    static async callExternalAI(projectId, month, year, reports) {
-        const AI_URL = '....';
-
+    static async callExternalAI(requestBody) {
+        const AI_URL = 'https://orange-space-adventure-4j6747vw4vvj2q59j-8000.app.github.dev/api/v1/analisis/iniciar';
+        // console.log("Body enviado:", requestBody);
         const response = await fetch(AI_URL, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                projectId,
-                period: `${month}/${year}`,
-                reports
-            })
+            body: JSON.stringify(requestBody )
         });
 
-        if (!response.ok) {
-            throw new Error(`AI_SERVICE_ERROR: ${response.status} ${response.statusText}`);
-        }
+        
+        // if (!response.ok) {
+        //     throw new Error(`AI_SERVICE_ERROR: ${response.status} ${response.statusText}`);
+        // }
         return await response.json();
     } catch(error) {
         console.error("Error en callExternalAI:", error.message);
