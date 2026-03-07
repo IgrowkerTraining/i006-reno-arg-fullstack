85c06ada349350ca060528f3287d4b04e7c31277
Marina Victoria Blanco <marivic_05@hotmail.com>
2026-03-07 14:10:28 -0300

    ReporteIAObra ok

--- apps/frontend/src/pages/ReporteIAObra.tsx
+++ apps/frontend/src/pages/ReporteIAObra.tsx
@@ -1,33 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Card } from "../components/common/Card";
-import { CheckCircle, Download, Eye, FileText, Flower, HardHat, UserRoundCog } from "lucide-react";
-import { ProgressBar } from "../components/common/ProgressBar";
+import { Download, Eye, FileText, Flower, HardHat, UserRoundCog } from "lucide-react";
 import { Button } from "../components/common/Button";
-
 import { useRef } from "react";
 import { handleExportPDF } from "../utils/exportReport";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation} from "react-router-dom";
+import { formatDate } from "../utils/formateDate";
 
 const ReporteIA: React.FC = () => {
 
-  const [loading, setLoading] = useState<boolean>(false);
   const [dataAnalysis, setDataAnalysis] = useState(null)
-  
+
   const reportRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
   const { analysis, month, year } = location.state || {};
 
-  useEffect(()=>{
-    if (analysis){
-      setDataAnalysis(analysis)
-    }  
-  },[])
+  useEffect(() => {
+    if (analysis) {
+      setDataAnalysis(analysis.data.analysis.resultado)
+    }
+  }, [analysis])
 
 
-  if (!analysis) {
+  if (!analysis || !dataAnalysis ) {
     return <p>No hay análisis disponible</p>;
   }
-  console.log({dataAnalysis});
+
+console.log({dataAnalysis});
 
 
   return (
@@ -35,46 +34,49 @@ const ReporteIA: React.FC = () => {
       <h3 className="flex items-center gap-2 text-secondary-plus font-bold text-2xl"><Flower size={20} /> ANÁLISIS ASISTIDO POR IA</h3>
       <header className="flex items-end gap-6 mt-4">
         <div>
-        <h2 className="font-bold text-2xl my-2">{}</h2>
-        <h4 className="font-bold">{} - Periodo: {month} | {year}</h4>
+          <h2 className="font-bold text-2xl my-2">{dataAnalysis.proyecto}</h2>
+          <h4 className="font-semibold">Periodo analizado: {month} | {year} | Fecha de generación: {formatDate(dataAnalysis.fecha_de_generacion)}</h4>
         </div>
-        <Button variant="outline" className="ml-auto" onClick={() =>""/*  handleExportPDF({ reportRef, analysis } ) */} data-html2canvas-ignore="true"><Download size={20} className="mr-2" />Exportar PDF</Button>
+        <Button variant="outline" className="ml-auto" onClick={() =>  handleExportPDF({ reportRef, dataAnalysis } ) } data-html2canvas-ignore="true"><Download size={20} className="mr-2" />Exportar PDF</Button>
       </header>
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6 ">
         <Card className="flex flex-col gap-4 p-8 rounded-xl border-neutro-3 bg-white" >
           <h4 className="font-bold text-xl"><FileText size={25} className="inline mr-2 text-secondary" />Resumen General</h4>
-          <p className="">{analysis.resumen_general}</p>
-
-        </Card>
-        <Card className="flex flex-col gap-4 p-8 rounded-xl border-neutro-3 bg-white" >
-          <h4 className="font-bold text-xl"><FileText size={25} className="inline mr-2 text-accent" />Ejecución y Avance</h4>
-          <div className="flex justify-between text-sm mt-2">
-            <span>Avance de obra</span>
-            <span>45%</span>
+            <div className="border-l-4 border-secondary pl-4">          
+          <p>{dataAnalysis.resumen_general_del_estado_de_la_obra}</p>
           </div>
-          <ProgressBar value={45} color="bg-accent" />
-          <p className="">{analysis.ejecucion_planificacion}</p>
 
         </Card>
         <Card className="flex flex-col gap-4 p-8 rounded-xl border-neutro-3 bg-white" >
-          <h4 className="font-bold text-xl"><HardHat size={25} className="inline mr-2 text-primary" />Cumplimiento y seguridad</h4>
-          <p className="bg-secondary p-2 rounded-lg text-white flex items-center justify-center">{analysis.cobertura_art === "Activa" ? <><CheckCircle className="inline mr-2" size={16} /> ART VIGENTE: Provincia ART</> : "Cobertura inactiva"}</p>
-          <p className="">{analysis.seguridad_cumplimiento}</p>
+          <h4 className="font-bold text-xl"><FileText size={25} className="inline mr-2 text-accent" />Ejecución y Planificación</h4>
+          <div className="border-l-4 border-accent pl-4">
+            <p >{dataAnalysis.ejecucion_y_planificacion}</p>
+          </div>
+
+        </Card>
+        <Card className="flex flex-col gap-4 p-8 rounded-xl border-neutro-3 bg-white" >
+          <h4 className="font-bold text-xl"><HardHat size={25} className="inline mr-2 text-primary" />Medidas de seguridad y cumplimiento</h4>
+          <div className="border-l-4 border-primary pl-4">
+            <p>{dataAnalysis.medidas_de_seguridad_y_cumplimiento}</p>
+          </div>
 
 
         </Card>
         <Card className="flex flex-col gap-4 p-8 rounded-xl border-neutro-3 bg-white" >
           <h4 className="font-bold text-xl"><UserRoundCog size={25} className="inline mr-2 text-accent-2" />Validación técnica</h4>
-          <p className="">{analysis.validacion_tecnica}</p>
-     
+          <div className="border-l-4 border-accent-2 pl-4">
+            <p>{dataAnalysis.validaciones_tecnicas}</p>
+          </div>
+
         </Card>
-        <Card className="flex flex-col gap-4 p-8 rounded-xl bg-[#5373DE] border-primary col-span-2 text-white" >
+        <Card className="flex flex-col gap-4 p-8 rounded-xl bg-[#E5EAFA] border-[#5373DE] col-span-2 text-[#5373DE]" >
           <h4 className="font-bold text-xl"><Eye size={25} className="inline mr-2 " />Observación General</h4>
-          <p className="">{analysis.observacion_general}</p>
+          <p>{dataAnalysis.observacion_general}</p>
         </Card>
       </section>
-    </div> )}
- 
+    </div>)
+}
+
 
 
 export default ReporteIA;
